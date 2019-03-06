15c4fd455f1e359946f976566ff141434f1a770c
Hannah Sutton-Molnar <hanniesutton@gmail.com>
2019-03-06 18:56:41 +0000

    characterIndex exercise

---
+++ characterIndex.js
@@ -0,0 +1,22 @@
+function characterIndex(string) {
+    var outObject = {}
+
+    for (var char in string) {
+        if (string[char] !== ' ') {
+            outObject[string[char]] = [];
+        }
+    }
+
+    for (var char in string) {
+        if (string[char] !== ' '){
+        outObject[string[char]].push(char);
+        }
+    }
+
+    var instances = 0;
+
+    console.log(outObject);
+}
+
+characterIndex('lighthouse in the house');
+
